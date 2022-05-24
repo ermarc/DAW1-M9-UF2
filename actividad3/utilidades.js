b0f9296d3b87e6f1b1e916cb930619083d6f68e1
ermarc <91552736+ermarc@users.noreply.github.com>
2022-05-24 09:19:38 +0200

    Delete actividad3 directory

--- actividad3/utilidades.js
+++
@@ -1,9 +0,0 @@
-const inicioHover = document.querySelectorAll(".vid");
-for (let i = 0; i < inicioHover.length; i++) {
-    inicioHover[i].addEventListener('mouseenter', function() {
-        inicioHover[i].play()
-    });
-    inicioHover[i].addEventListener('mouseout', function() {
-        inicioHover[i].pause()
-    })
-}
